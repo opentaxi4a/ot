9d10ee3b3a17228ef88472f8ff7995aa55dcc885
Babak Alizadeh <alizadeh.babak@gmail.com>
2025-01-25 22:19:50 -0800

    progress in home page

--- src/pages/app/base.vue
+++ src/pages/app/base.vue
@@ -1,14 +1,14 @@
 <template>
   <v-navigation-drawer v-model="drawer">
-    <!--  -->
+    
   </v-navigation-drawer>
 
-  <v-app-bar color="deep-orange">
+  <v-app-bar color="indigo-darken-2">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-app-bar-title>{{ $t('app.name') }}</v-app-bar-title>
   </v-app-bar>
 
-  <v-main class="ma-0 pa-0">
+  <v-main class="">
     <RouterView />
   </v-main>
 </template>
@@ -16,7 +16,7 @@
 <script>
 export default {
   data: () => ({
-    drawer: null
+    drawer: false
   }),
 }
 </script>
